b10fb66b53d14e131d02f5ff35954a55a0325a46
shemuel-1 <samuelpaintsil246@gmail.com>
2025-07-15 19:25:22 +0000

    Week 2 Document Object Model

---
+++ week02/scripts/script.js
@@ -0,0 +1,26 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function () {
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = ""; // Clear input after adding
+        input.focus();
+    }
+    else {
+        alert("Please enter a chapter name.");
+    }
+});
